e0a1202daa291e50c1f946bdd4963ca411f75330
fllorisheredia <fllorisheredia@gmail.com>
2025-05-07 14:26:27 +0200

    Se ha anadido la funcion de guardar los productos en cache una vez obtenidos de la bd para si evitar multiples peticiones http

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,55 +1,65 @@
-
-
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Product, ProductsResponse } from '@products/interfaces/product.interface';
-import { Observable, tap } from 'rxjs';
+import {
+  Product,
+  ProductsResponse,
+} from '@products/interfaces/product.interface';
+import { delay, Observable, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
+// import { ProductsResponse, Product } from '../interfaces/product.interface';
 
+const baseUrl = environment.baseUrl;
 
-const baseUrl = environment.baseUrl
-
-interface Options{
+interface Options {
   limit?: number;
   offset?: number;
   gender?: string;
 }
 
-
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class ProductService {
+  private _http = inject(HttpClient);
 
-
-
-  private _http =inject(HttpClient);
-
-
-
+  private _productsCache = new Map<string, ProductsResponse>(); // Cache para guardar los productos que ya se han traido de la BD
+  private _productCache = new Map<string, Product>(); // Se guarda el cache de un producto en especifico
   getProducts(options: Options): Observable<ProductsResponse> {
+    const { limit = 9, offset = 0, gender = '' } = options;
 
+    const key = `${limit}-${offset}-${gender}`; // Cache key 9-0-''
 
-    const{ limit =9, offset=0, gender=''} = options;
+    if (this._productsCache.has(key)) {
+      return of(this._productsCache.get(key)!);
+    }
 
-
-
-    return this._http.get<ProductsResponse>
-    (`${baseUrl}/products`, {
-      params:{
-        limit,
-        offset,
-        gender,
-      },
-    })
-    .pipe(tap((resp) => console.log(resp)));
-
+    return this._http
+      .get<ProductsResponse>(`${baseUrl}/products`, {
+        params: {
+          limit,
+          offset,
+          gender,
+        },
+      })
+      .pipe(
+        tap((resp) => console.log(resp)),
+        tap((resp) => this._productsCache.set(key, resp)) // se mandas los valores de la respuesta, se guardan en el cache
+      );
   }
-
 
   getProductbyIdSlug(idSlug: string): Observable<Product> {
 
-    return this._http.get<Product>(`${baseUrl}/products/${idSlug}`);
+    if(this._productCache.has(idSlug)){
+      return of(this._productCache.get(idSlug)!)// Se obtiene el producto del cache
+    }
 
 
+
+
+
+//Se devuelve el producto por us ID/Slug
+    return this._http.get<Product>(`${baseUrl}/products/${idSlug}`).
+    pipe(
+      delay(2000), //tiempo de espera
+      tap((product)=> this._productCache.set(idSlug, product)), // Se guarda el producto en el cache
+    )
   }
-
 }
